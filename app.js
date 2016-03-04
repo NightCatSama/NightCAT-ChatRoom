fc1c65a2201dbe886f4984e129e88e56b34f5be4
陈慕杰 <642163903@qq.com>
2016-03-04 08:34:39 +0800

    fixed style

--- app.js
+++ app.js
@@ -1,96 +1,96 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
 //  hat模块生成不重复ID
-var hat = require('hat');
-var rack = hat.rack();
+var hat = require('hat')
+var rack = hat.rack()
 
-var routes = require('./routes/index');
+var routes = require('./routes/index')
 
 //  mongoDB module
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/Users');
+var mongo = require('mongodb')
+var monk = require('monk')
+var db = monk('localhost:27017/Users')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
+app.use('/', routes)
 
 //  用户登录
 app.post('/login',function(req,res){
-  var collection = db.get("userdata");
-  var username = req.body.username;
-  var password = req.body.password;
+  var collection = db.get("userdata")
+  var username = req.body.username
+  var password = req.body.password
 
   collection.find({
     "username" : username
   },function(err,docs){
     if(docs==''||docs==null){
-      console.log("用户名不存在!");
-      res.send('用户名不存在');
-      res.end();
+      console.log("用户名不存在!")
+      res.send('用户名不存在')
+      res.end()
     }else {
       if(docs[0].password == password){
-      res.send(docs[0]);
-      res.end();
+      res.send(docs[0])
+      res.end()
       }else {
-        console.log("密码错误!");
-        res.send('密码错误');
-        res.end();
+        console.log("密码错误!")
+        res.send('密码错误')
+        res.end()
       }
     }
   })
-});
+})
 
 //  注册用户
 app.post('/inputData',function(req,res){
 
-  var collection = db.get("userdata");
-  var username = req.body.username;
-  var password = req.body.password;
+  var collection = db.get("userdata")
+  var username = req.body.username
+  var password = req.body.password
   collection.find({
     "username" : username
   },function(err,docs){
     if(docs==''||docs==null){
-      var UID = rack();
+      var UID = rack()
       collection.insert({
         "username" : username,
         "password" : password,
         "UID" : UID
-      });
-      res.send(username);
-      res.end();
+      })
+      res.send(username)
+      res.end()
     } else {
-      console.log("用户名已存在");
-      res.send('用户名已存在');
-      res.end();
+      console.log("用户名已存在")
+      res.send('用户名已存在')
+      res.end()
     }
-  });
+  })
 })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
@@ -98,23 +98,23 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,43 @@
-var url = require("url");
-var queryString  = require("querystring");
-var express = require('express');
-var router = express.Router();
+var url = require("url")
+var queryString  = require("querystring")
+var express = require('express')
+var router = express.Router()
 
-var flag = false;
+var flag = false
 
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/Users');
+var mongo = require('mongodb')
+var monk = require('monk')
+var db = monk('localhost:27017/Users')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{alerttext : ""});
-});
+  res.render('index',{alerttext : ""})
+})
 
 router.get('/chatroom', function(req, res, next) {
-	var username = '';
-	var collection = db.get("userdata");
-	var stringA = req.originalUrl;
-	var UID =  stringA.substr(stringA.indexOf("?")+5)
-	if(UID!=''&&UID!=null){flag=true;}
+	var queryUrl = url.parse(req.url).query
+	var username = ''
+	var collection = db.get("userdata")
+	var UID =  queryString.parse(queryUrl).UID
+	if(UID!=''&&UID!=null){flag=true}
 	collection.find({
     	"UID" : UID
 	},function(err,docs){
 	  if(docs==''||docs==null){
-	  	flag=false;
+	  	flag=false
 		} 
 	else { username = docs[0].username }
-	});
+	})
 	setTimeout(function(){
     if(flag){
-    res.render('chatroom',{ username : username });
+    res.render('chatroom',{ username : username })
 	}
-	else {res.redirect('error');}
+	else {res.redirect('error')}
 	},200)
-});
+})
 
 router.use('/register',function(req,res,next) {
-	res.render('register');
-});
+	res.render('register')
+})
 
-module.exports = router;
+module.exports = router
